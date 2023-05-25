6bf5e09cfd84762e0a62f2e475c033063116cd3c
iamshiv007 <iamshiv20032003@gmail.com>
2023-05-25 18:14:09 +0530

    fix some issue

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -11,7 +11,9 @@ const NewFeatures = ({ imgUrl, title, subtitle }) => (
     <h1 className="mt-[26px] font-bold text-[24px] leading-[30.24px] text-white">
       Title {title}
     </h1>
-    <p className="flex-1 mt-[16px] font-normal text-[18px] text-[#B0B0B0] leading-[32.4px]"></p>
+    <p className="flex-1 mt-[16px] font-normal text-[18px] text-[#B0B0B0] leading-[32.4px]">
+      {subtitle}
+    </p>
   </div>
 );
 
